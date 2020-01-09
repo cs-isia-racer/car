8da336d715abf3cebabf9a7ea61cf7f68e1fd7df
nockty <corentin.dupret@student.ecp.fr>
2020-01-09 16:41:18 +0100

    add wiringpy_mock

---
+++ wiringpi_mock.py
@@ -0,0 +1,27 @@
+class GPIO:
+    PWM_OUTPUT = 0
+    PWM_MODE_MS = 0
+
+
+def wiringPiSetupGpio():
+    pass
+
+
+def pinMode(*args, **kwargs):
+    pass
+
+
+def pwmSetMode(*args, **kwargs):
+    pass
+
+
+def pwmSetClock(*args, **kwargs):
+    pass
+
+
+def pwmSetRange(*args, **kwargs):
+    pass
+
+
+def pwmWrite(*args, **kwargs):
+    pass
